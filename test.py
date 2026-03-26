c87fb4e526bcd04a8babdd8fe267dc16d133d9c1
Quintustp11 <marchandq41@gmail.com>
2026-03-26 17:38:03 +0100

    Premiers tests de simulation (compréhension du code)

--- test.py
+++ test.py
@@ -1,1 +1,116 @@
-import rocketpy
+import rocketpy as rc
+
+import datetime
+
+tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+
+env = rc.Environment(latitude=45.6, longitude=-88, elevation=1400)
+
+env.set_atmospheric_model(type="standard_atmosphere")
+
+env.info()
+
+Pro75M1670 = rc.HybridMotor(
+    thrust_source="../data/motors/cesaroni/Cesaroni_M1670.eng",
+    dry_mass=1.815,
+    dry_inertia=(0.125, 0.125, 0.002),
+    nozzle_radius=33 / 1000,
+    grain_number=5,
+    grain_density=1815,
+    grain_outer_radius=33 / 1000,
+    grain_initial_inner_radius=15 / 1000,
+    grain_initial_height=120 / 1000,
+    grain_separation=5 / 1000,
+    grains_center_of_mass_position=0.397,
+    center_of_dry_mass_position=0.317,
+    nozzle_position=0,
+    burn_time=3.9,
+    throat_radius=11 / 1000,
+    coordinate_system_orientation="nozzle_to_combustion_chamber",
+)
+
+Pro75M1670.info()
+
+calisto = rc.Rocket(
+    radius=127 / 2000,
+    mass=14.426,
+    inertia=(6.321, 6.321, 0.034),
+    power_off_drag="../data/rockets/calisto/powerOffDragCurve.csv",
+    power_on_drag="../data/rockets/calisto/powerOnDragCurve.csv",
+    center_of_mass_without_motor=0,
+    coordinate_system_orientation="tail_to_nose",
+)
+
+calisto.add_motor(Pro75M1670, position=-1.255)
+
+rail_buttons = calisto.set_rail_buttons(
+    upper_button_position=0.0818,
+    lower_button_position=-0.6182,
+    angular_position=45,
+)
+
+nose_cone = calisto.add_nose(
+    length=0.55829, kind="von karman", position=1.278
+)
+
+fin_set = calisto.add_trapezoidal_fins(
+    n=4,
+    root_chord=0.120,
+    tip_chord=0.060,
+    span=0.110,
+    position=-1.04956,
+    cant_angle=0.5,
+    airfoil=("../data/airfoils/NACA0012-radians.txt","radians"),
+)
+
+tail = calisto.add_tail(
+    top_radius=0.0635, bottom_radius=0.0435, length=0.060, position=-1.194656
+)
+
+main = calisto.add_parachute(
+    name="main",
+    cd_s=10.0,
+    trigger=800,      # ejection altitude in meters
+    sampling_rate=105,
+    lag=1.5,
+    noise=(0, 8.3, 0.5),
+    radius=1.5,
+    height=1.5,
+    porosity=0.0432,
+)
+
+drogue = calisto.add_parachute(
+    name="drogue",
+    cd_s=1.0,
+    trigger="apogee",  # ejection at apogee
+    sampling_rate=105,
+    lag=1.5,
+    noise=(0, 8.3, 0.5),
+    radius=1.5,
+    height=1.5,
+    porosity=0.0432,
+)
+
+test_flight = rc.Flight(
+    rocket=calisto, environment=env, rail_length=5.2, inclination=85, heading=0
+    )
+
+test_flight.prints.initial_conditions()
+
+test_flight.prints.surface_wind_conditions()
+
+test_flight.prints.launch_rail_conditions()
+
+test_flight.prints.out_of_rail_conditions()
+
+test_flight.prints.burn_out_conditions()
+
+test_flight.prints.apogee_conditions()
+
+test_flight.prints.events_registered()
+
+test_flight.prints.impact_conditions()
+
+test_flight.prints.maximum_values()
+
+test_flight.plots.trajectory_3d()
