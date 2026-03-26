6b99fae194c62c9429f6cc36b40d188b84b9a44e
bibossrou <bibossrou@gmail.com>
2026-03-26 23:54:55 +0100

    tuning du PID, performant (rafale + vent constant)

--- test_pid.py
+++ test_pid.py
@@ -34,29 +34,66 @@ class PIDController:
             
             return output
 
+masse = 1     # Inertie de ton système
 
-#bonne valeur du pid, erreur à 5%
-Kp = 0.001
-Ki = 0.000
-Kd = 0.0005
+'''
+#bonne valeur du pid, faible erreur mais trop lent
+Kp = 1.5*masse
+Ki = 0.5*masse
+Kd = 15*masse
+'''
+'''
+#bonne valeur du PID, réactif et erreur faible
+Kp = 10*masse
+Ki = 0.5*masse
+Kd = 15*masse
+'''
+Kp = 40*masse
+Ki = 50*masse
+Kd = 30*masse
+
+
 L = []
 
 start = time.perf_counter()
 PID = PIDController(Kp, Ki, Kd, 0) #0 pour l'angle attendue
-x0 = -3 #l'angle qu'on a
-x = x0
-dt = 0.01
+x0 = -5 #l'angle qu'on a
+
 L.append(x0)
-for i in range(10):
-    x = PID.compute(x, dt)
-    L.append(x)
+# Initialisation physique
+x = x0         # Position (angle)
+vitesse = 0     # Vitesse angulaire
+
+dt = 0.02
+
+for i in range(500): # On augmente un peu car l'inertie ralentit tout
+    # 1. Le PID calcule la FORCE à appliquer
+    force = PID.compute(x, dt)
+    
+    # 2. Physique : Accélération = Force / Masse
+
+
+    vent_constant = 0.1*i #m/s vent augmentant avec l'altitude
+
+    acceleration = (force+vent_constant) / masse
+    
+    # 3. Physique : Vitesse = Vitesse + Accélération * dt
+    vitesse += acceleration * dt
+
+    
+    # Simulation d'une rafale de vent à l'itération 50
+    if i == 50:
+        vitesse += 45.0  # On donne un gros coup de pouce soudain
+        print(f"--- RAFALE DE VENT ! {vitesse} m/s ---")
+    
+    
+    # 4. Physique : Position = Position + Vitesse * dt
+    x += vitesse * dt
     print(x)
-end = time.perf_counter()
+    L.append(x)
 
+end =  time.perf_counter()
 print(f"temps exec : {end-start} secondes")
-
-
-
 
 plt.plot(L, marker='o', linestyle='-', label='Angle (process variable)')
 
@@ -66,13 +103,19 @@ plt.xticks(range(len(L)))
 # Piste pour la clarté : ajouter une ligne horizontale pour la consigne (setpoint)
 plt.axhline(y=0, color='r', linestyle='--', label='Setpoint')
 
-plt.axhline(y=x0/10, color='r', linestyle='-', label='lim +')
-plt.axhline(y=-x0/10, color='r', linestyle='-', label='lim -')
+for k in range(1, 11):
+    if k == 1:
+        plt.axvline(x = k/dt, color='b', linestyle='--', label='repère (secondes)')
+
+    else: plt.axvline(x = k/dt, color='b', linestyle='--')
+
+plt.axhline(y=x0/20, color='r', linestyle='-', label='lim erreur 5%')
+plt.axhline(y=-x0/20, color='r', linestyle='-')
 
 
 plt.xlabel('Itérations')
 plt.ylabel('Angle')
 plt.title('Convergence du PID')
 plt.legend()
-plt.grid(True)
+plt.grid(False)
 plt.show()
