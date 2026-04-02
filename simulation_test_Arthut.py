1c30c862c0ab1c6d935323ad92b6c534041e1f5a
Arthur MILLET <arthur.millet@ipsa.fr>
2026-04-02 14:49:17 +0200

    Chnagement de la masse de la fusée, et de la position pour garder une stabilité optimale

--- simulation_test_Arthut.py
+++ simulation_test_Arthut.py
@@ -21,13 +21,14 @@ tank_shape = rocketpy.CylindricalTank(radius= 0.15, height = 1.0) #forme du tank
 tank_oxidizer = rocketpy.MassFlowRateBasedTank( #le conteneur du carbu liquide
 name = 'Tank_test',
 geometry = tank_shape,
-flux_time= 10.0,
+flux_time= 20.0,
 liquid = oxidizer_liq,
 gas = oxidizer_gaz,
-initial_liquid_mass= 3.0,
+initial_liquid_mass= 1.5,
 initial_gas_mass= 0,
 liquid_mass_flow_rate_in= 0,
-liquid_mass_flow_rate_out= (3.0-0.1)/10,
+liquid_mass_flow_rate_out= (1.5-0.01)/20.0, #il faut mettre une valeur un peu en dessous de la masse initial du liquide, d'où le -0.01
+# liquid_mass_flow_rate_out = (tank_oxidizer.initial_liquid_mass - 0.01)/tank_oxidizer.flux_time,
 gas_mass_flow_rate_in= 0,
 gas_mass_flow_rate_out=0,
 )
@@ -35,8 +36,8 @@ gas_mass_flow_rate_out=0,
 
 
 MoteurTest = rocketpy.HybridMotor( #valeurs un peu aléatoires pour voir ce que ça fait, et les modifier pour observer.
-thrust_source= 1500,
-dry_mass= 3.0,
+thrust_source= 350,
+dry_mass= 1.5,
 dry_inertia = (0.125, 0.125, 0.002),
 nozzle_radius= 0.11,
 grain_number= 8,
@@ -48,10 +49,10 @@ grain_density= 900,
 grains_center_of_mass_position=0.410,
 center_of_dry_mass_position= 0.200,
 nozzle_position= -0.6,
-burn_time= 10,
+burn_time= 20.0,
 throat_radius=  0.04)
 
-MoteurTest.add_tank(tank_oxidizer, position= 1.0615)
+MoteurTest.add_tank(tank_oxidizer, position= 1.160)
 
 # MoteurTest.all_info()
 
@@ -59,7 +60,7 @@ MoteurTest.add_tank(tank_oxidizer, position= 1.0615)
 
 fusee_essai = rocketpy.Rocket( #valeur un peu aléatoire, encore pour voir ce que la simulation fait.
     radius= 0.17,
-    mass = 15,
+    mass = 5,
     inertia = (6.321, 6.321, 0.034), #aucne idée de ce que ceci fait exactement
     power_on_drag= "./test_PowerOnDrag.csv",
     power_off_drag= "./test_powerOffDrag.csv",
@@ -112,9 +113,11 @@ Fin de la définition de la fusée.
 
 
 #fusee_essai.plots.all() #soyons fous et dessinons tous ce qu'on peut dessiner
-fusee_essai.plots.static_margin() #il faut une valeure positive pas trop élevée --> plus c'est élevé plus la fusée sera stable, mais si c'est trop élevé la simulation ne marchera aps
+#fusee_essai.plots.static_margin() #il faut une valeure positive entre 1.5 et 2 --> plus c'est élevé plus la fusée sera stable, mais si c'est trop élevé la simulation ne marchera aps
 
-# fusee_essai.all_info()
+#fusee_essai.all_info()
+
+
 
 
 
@@ -122,13 +125,17 @@ fusee_essai.plots.static_margin() #il faut une valeure positive pas trop élevé
 Début de la simulation.
 """
 
+
+
+'''
 vol_simu = rocketpy.Flight(
     rocket = fusee_essai, environment = env, rail_length = 4.0, inclination = 80, heading=  90)
 
-vol_simu.info()
-vol_simu.plots.trajectory_3d()
+#vol_simu.info()
+#vol_simu.plots.trajectory_3d()
+vol_simu.all_info()
 
-
+'''
 
 ### Analysis of the simulation:
 
