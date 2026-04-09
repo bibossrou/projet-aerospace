e1647faf3ce420bf1d083fa4755f0a61f37372c9
Arthur MILLET <arthur.millet@ipsa.fr>
2026-04-09 14:18:37 +0200

    queqlueq modif

--- simulation_test_Arthut.py
+++ simulation_test_Arthut.py
@@ -16,8 +16,7 @@ env.max_expected_height = 4000 #hauteur maximum de 3000 mètres, ceci va diminue
 oxidizer_liq = rocketpy.Fluid(name = "Protoxide d'azote", density = 1223) #notre oxidizant liquide, du protoxide d'azote
 oxidizer_gaz = rocketpy.Fluid(name = 'test_fluide_gaz', density = 1.80) #notre oxidisant gaz
 
-tank_shape = rocketpy.CylindricalTank(radius= 0.04, height = 0.8) #forme du tank, dans ce cas cylindrical de hauteur 1m et diametre 30 cm.
-
+tank_shape = rocketpy.CylindricalTank(radius= 0.04, height = 0.1) 
 
 tank_oxidizer = rocketpy.MassFlowRateBasedTank( #le conteneur du carbu liquide
 name = 'Tank_test',
@@ -29,7 +28,6 @@ initial_liquid_mass= 0.315, #en utilisant les svaleurss du moteur en question.
 initial_gas_mass= 0,
 liquid_mass_flow_rate_in=0 ,
 liquid_mass_flow_rate_out= (0.315-0.0000001)/8.4, #il faut mettre une valeur un peu en dessous de la masse initial du liquide, d'où le -0.01
-# liquid_mass_flow_rate_out = (tank_oxidizer.initial_liquid_mass - 0.01)/tank_oxidizer.flux_time,
 gas_mass_flow_rate_in= 0,
 gas_mass_flow_rate_out=0,
 )
@@ -41,20 +39,20 @@ thrust_source= "./RATT_K240H.csv", #changer le nom du csv
 interpolation_method= "linear",
 dry_mass= (2.814-1.293),
 dry_inertia = (0.105, 0.105, 0.00138), # Inertie centre de masse quand tout le carburant est recgargé, calculé en utilisant l'aide de différentes ia.    
-nozzle_radius= 0.020, #approx 4 x pi x throat_radiuss^2.
+nozzle_radius= 0.020, 
 grain_number= 6,
-grain_separation= 0,
-grain_outer_radius= 0.058,
-grain_initial_inner_radius= 0.048,
-grain_initial_height= (0.908/8),
+grain_separation= 0.001,
+grain_outer_radius= 0.050,
+grain_initial_inner_radius= 0.035,
+grain_initial_height= (0.908/6),
 grain_density= 900,
-grains_center_of_mass_position=0.380,
-center_of_dry_mass_position= 0.220,
-nozzle_position= -0.10,
+grains_center_of_mass_position=0.680,
+center_of_dry_mass_position= 0.520,
+nozzle_position= -0.06,
 burn_time= 8.4,
 throat_radius=  0.01)
 
-MoteurTest.add_tank(tank_oxidizer, position= 1.150)
+MoteurTest.add_tank(tank_oxidizer, position= 1.35)
 
 #MoteurTest.all_info()
 #MoteurTest.plots.all()
@@ -72,23 +70,23 @@ fusee_essai = rocketpy.Rocket( #valeur un peu aléatoire, encore pour voir ce qu
     coordinate_system_orientation= "tail_to_nose"
     )
 
-fusee_essai.add_motor(MoteurTest, position= -0.6)
+fusee_essai.add_motor(MoteurTest, position= -0.38)
 
 coiffe = fusee_essai.add_nose(length=0.25, kind = 'conical', position = 1.278) #tangent kind of cone looks the best
 
 fin_groupe = fusee_essai.add_trapezoidal_fins(
     n = 4,
-    span = 0.15,
-    root_chord= 0.2,
-    tip_chord= 0.03,
-    position = -0.4,
+    span = 0.20,
+    root_chord= 0.18,
+    tip_chord= 0.04,
+    position = -0.40+0.15 + 0.02 +0.1,
     cant_angle= 4)
 
 tail = fusee_essai.add_tail(
-    top_radius= 0.125/2,
-    bottom_radius= (0.125+0.05)/2,
-    position = -0.6,
-    length= 0.105)
+    top_radius= 0.110/2,
+    bottom_radius= (0.110+0.05)/2,
+    position = -0.4+0.1,
+    length= 0.08)
 
 
 
@@ -100,18 +98,18 @@ def parachute_tiré(p, h, y):
     
 
 parachute_drogue = fusee_essai.add_parachute( #premier parachute, il va être déployé à l'apogee. 
-    cd_s = 0.5,
+    cd_s = 0.1,
     name = "drogue_parachute",
     trigger= "apogee" #il s'active à l'apogee
 )
 
 parachute = fusee_essai.add_parachute(
     cd_s = 10,
-    name = "parachute",
+    name = "parachute_principal",
     trigger = parachute_tiré,
     sampling_rate= 100,
     lag = 1.0,
-    noise = (0, 20, 0.5))
+    noise = (0, 10, 0.3))
 
 
 fusee_essai.draw() #dessiner pour voir ce à quoi elle ressemble, avant d'ajouter tout le bazar.
@@ -139,11 +137,12 @@ Début de la simulation.
 
 
 vol_simu = rocketpy.Flight(
-    rocket = fusee_essai, environment = env, rail_length = 4.0, inclination = 90, heading=  270)
+    rocket = fusee_essai, environment = env, rail_length = 4.0, inclination = 90, heading=  270, name= "Projet nameless I")
 
-vol_simu.info()
-vol_simu.plots.trajectory_3d()
-#vol_simu.all_info()
+#vol_simu.info()
+#vol_simu.plots.trajectory_3d()
+vol_simu.all_info()
+
 
 
 
@@ -152,4 +151,4 @@ vol_simu.plots.trajectory_3d()
 #apogee_by_mass(vol_simu, min_mass = 7, max_mass = 15, points = 5, plot = True)
 
 
-#liftoff_speed_by_mass(flight = vol_simu, min_mass = 7, max_mass = 15, points= 5, plot = True)
+#liftoff_speed_by_mass(flight = vol_simu, min_mass = 7, max_mass = 15, points= 5, plot = True)'''
